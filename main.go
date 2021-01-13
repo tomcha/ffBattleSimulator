3c4dd91b404fde480b4a54e7679a8ac980a452dd
tomcha <tomcha@tomcha.net>
2021-01-13 18:21:21 +0900

    yaml fileからの読み込みを実装

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"strconv"
 	"time"
+
+	"github.com/go-yaml/yaml"
 )
 
 type Character struct {
@@ -39,6 +43,22 @@ func (m *Monster) calcDamaged() {
 }
 
 func main() {
+
+	// monster.yamlを []byte として読み込みます。
+	buf, err := ioutil.ReadFile("./monster.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// []byte を []map[string]string に変換します。
+	data, err := ReadOnSliceMap(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	numberOfMonsterTypes := len(data)
+
 	var c Character
 	c.name = "Tomcha"
 	c.skill = roleDice(1, 6) + 6
@@ -47,16 +67,35 @@ func main() {
 
 	fmt.Println(c)
 
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(numberOfMonsterTypes)
+
 	var m Monster
-	m.name = "goblin"
-	m.skill = 5
-	m.stamina = 3
-	m.attack = 1
+	m.name = data[n]["name"]
+	m.skill, _ = strconv.Atoi(data[n]["skill"])
+	m.stamina, _ = strconv.Atoi(data[n]["stamina"])
+	m.attack, _ = strconv.Atoi(data[n]["attack"])
 
 	fmt.Println(m)
 	battle(&c, &m)
 	fmt.Println(c)
 	fmt.Println(m)
+}
+
+func readMonsterYamlFile() {
+}
+
+// yaml形式の[]byteを渡すと[]map[string]stringに変換してくれる関数です。
+func ReadOnSliceMap(fileBuffer []byte) ([]map[string]string, error) {
+	data := make([]map[string]string, 20)
+	// ここで変換を行っています。
+	// []byteを渡すとデータ型に合わせて上手い事マッピングしてくれます。
+	err := yaml.Unmarshal(fileBuffer, &data)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return data, nil
 }
 
 func roleDice(times int, hedron int) int {
