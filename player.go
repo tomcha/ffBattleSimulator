a1ff3c4333d373696285f61426ea28d58d820230
tomcha <tomcha@tomcha.net>
2023-12-15 09:24:57 +0900

    戦闘系関数実装

--- player.go
+++ player.go
@@ -19,9 +19,34 @@ func (p *Player) createNewCharactor(n string) {
 	p.luck = rand.Intn(6) + 7
 }
 
-func(p *Player) outputStatus(){
+func (p *Player) outputStatus() {
 	fmt.Printf("%s のステータス\n", p.name)
 	fmt.Printf("  skill   : %d\n", p.skill)
 	fmt.Printf("  stamina : %d\n", p.stamina)
 	fmt.Printf("  luck    : %d\n", p.luck)
+}
+
+func (p *Player) calcAttackPoint() (ap int) {
+	ap = rand.Intn(6) + rand.Intn(6) + 2 + p.skill
+	return
+}
+
+func (p *Player) calcDamage(dp int) (isDead bool) {
+	isDead = false
+	p.stamina -= dp
+	if p.stamina <= 0 {
+		isDead = true
+	}
+	return
+}
+
+func (p *Player) lucktest() (isLucky bool) {
+	judgePoint := rand.Intn(6) + rand.Intn(6) + 2
+	if judgePoint > p.luck {
+		isLucky = false
+	} else {
+		isLucky = true
+	}
+	p.luck -= 1
+	return
 }
