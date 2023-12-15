7ef0110761a1ad0d457148164ca459e6fb3c0350
tomcha <tomcha@tomcha.net>
2023-12-15 09:12:46 +0900

    Playerのステータス出力実装

--- player.go
+++ player.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 )
 
@@ -16,4 +17,11 @@ func (p *Player) createNewCharactor(n string) {
 	p.skill = rand.Intn(6) + 7
 	p.stamina = rand.Intn(6) + rand.Intn(6) + 14
 	p.luck = rand.Intn(6) + 7
+}
+
+func(p *Player) outputStatus(){
+	fmt.Printf("%s のステータス\n", p.name)
+	fmt.Printf("  skill   : %d\n", p.skill)
+	fmt.Printf("  stamina : %d\n", p.stamina)
+	fmt.Printf("  luck    : %d\n", p.luck)
 }
