97cc8143cd119b6a6962dfdd29a0fce5f3a3bf6b
tomcha <tomcha@tomcha.net>
2024-05-31 18:15:41 +0900

    戦闘を実装

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	fmt.Println("Game Start")
@@ -13,5 +16,29 @@ func main() {
 
 	enemy := ml.selectMonster()
 	fmt.Printf("Enemy name: %s\n", enemy.Name)
+	round := 1
+	for !(p.isDead()) {
+		fmt.Printf("Round %d fight!\n", round)
+		hap := p.calcAttackPoint()
+		eap := enemy.SkillPoint + rand.Intn(6) + 1 + rand.Intn(6) + 1
+		if eap < hap {
+			fmt.Printf("%s 's Attack is hit!\n", p.name)
+			enemy.StaminaPoint -= 2
+		} else if eap > hap {
+			fmt.Printf("%s 's Attack is hit!\n", enemy.Name)
+			p.stamina -= enemy.AttackPoint
+		} else {
+			fmt.Println("Attack is miss")
+		}
+
+		if enemy.StaminaPoint <= 0 {
+			fmt.Printf("%s is dead! You win!\n", enemy.Name)
+			break
+		}
+		round += 1
+	}
+	if p.isDead() {
+		fmt.Printf("%s is dead! You lose!\n")
+	}
 	fmt.Println("Game End")
 }
