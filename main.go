1065af837314e40f293a56d378e5624e88fc5a35
tomcha <tomcha@tomcha.net>
2024-05-31 17:49:12 +0900

    これから戦闘を実装

--- main.go
+++ main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Game Start")
+	p := Player{}
+	p.createNewCharactor("tomcha")
+	p.outputStatus()
+
+	ml := monsterList{}
+	ml.createMonster()
+
+	enemy := ml.selectMonster()
+	fmt.Printf("Enemy name: %s\n", enemy.Name)
+	fmt.Println("Game End")
 }
